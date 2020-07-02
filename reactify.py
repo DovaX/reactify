8893d2e3ae1aa729b9361b44015ec6449f176c7a
Dominik Vach <vach.dominik@gmail.com>
2020-07-02 16:39:15 +0200

    gui npm install, start, create-react-app

--- reactify.py
+++ reactify.py
@@ -1,16 +1,31 @@
 import os
 
-
-
 def npm_start():
+    try:
+        os.chdir(".//my-app")
+    except:
+        print("Already in right folder")
     os.system("npm start")
+    os.chdir("..")
+    
+def npm_install():
+    os.system("npm install")
+    
+    
+def npx_create_react_app():
+    os.system("npx create-react-app my-app")
+
+        
+############ DOGUI ##############        
+import dogui.dogui_core as dg
 
 
+#gui1=dg.GUI()
 
-
-class Tag:
-    def __init__(self):
-        pass
+#btn1=dg.Button(gui1.window,"npm start",npm_start,1,1)
+#btn2=dg.Button(gui1.window,"npx create react app",npx_create_react_app,1,2)
+#btn2=dg.Button(gui1.window,"npm install",npm_install,1,3)
+#gui1.build_gui()
 
 
 class Div(Tag):
